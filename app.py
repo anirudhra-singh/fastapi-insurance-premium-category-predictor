1b1f263a3132a1066238b389f6b48e09c82dbffc
anirudhra <anirudhra-singh@users.noreply.github.com>
2026-05-01 00:40:58 +0530

    fix userinput and computed fields bug

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from pydantic import Basemodel , Field, computed_field
-from typing import Literal,Annotated
+from pydantic import BaseModel , Field, computed_field
+from typing import Literal, Annotated
 import pickle
 import pandas as pd 
 from fastapi.responses import JSONResponse
@@ -9,7 +9,8 @@ from fastapi.responses import JSONResponse
 with open('model.pkl','rb') as f:
     model = pickle.load(f)
 
-app = FastAPI
+app = FastAPI()
+
 tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
 tier_2_cities = [
     "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
@@ -20,68 +21,66 @@ tier_2_cities = [
     "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
 ]
 
+# pydantic model to validate incoming data
+class UserInput(BaseModel):
 
-#pydantic model to validate incoming data
-class UserInput(Basemodel):
-
-    age: Annotated[int, Field(...,gt=0 ,lt=120, description='age of the user')]
-    weight: Annotated[float,Field(..., gt=0,description='weight of the user')]
-    height:Annotated[float,Field(..., gt=0,lt=2.5,description='height of the user')]
-    income_lpa:Annotated[float,Field(..., gt=0,description='Annual salary of the user')]
-    smoker:Annotated[bool,Field(..., description='is user a smoker')]
-    city:Annotated[str ,Field(...,description='city that the user belongs to')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='age of the user')]
+    weight: Annotated[float, Field(..., gt=0, description='weight of the user')]
+    height: Annotated[float, Field(..., gt=0, lt=2.5, description='height of the user')]
+    income_lpa: Annotated[float, Field(..., gt=0, description='annual salary of the user in lpa')]
+    smoker: Annotated[bool, Field(..., description='is user a smoker')]
+    city: Annotated[str, Field(..., description='city that the user belongs to')]
     occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
-
-
-@computed_field
-@property
-def bmi(self) -> float:
-    return self.weitght/(self.height**2)
-
-@computed_field
-@property
-def lifestyle_risk(self) -> str:
-  if self.smoker["smoker"] and self.bmi["bmi"]> 30:
-    return "high"
-  elif self.smoker["smoker"] or self.bmi["bmi"]>27:
-        return "medium"
-  else:
-    return "low"
-  
-
-@computed_field
-@property 
-def age_group(self) -> str:
-   if self.age < 25:
-      return "young"
-   elif self.age < 50:
-      return"Adult"
-   elif self.age < 60:
-      return "middle_aged"
-   return "senior"
-
-@computed_field
-@property
-def city_tier(self) -> int:
-   if self.city in tier_1_cities:
-    return 1
-   elif self.city in tier_2_cities:
-    return 2
-   else:
-    return 3
-
-  # prediction endpoint
-@app.post('/predicte')
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='occupation of the user')]
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self) -> str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"
+        
+    @computed_field
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "middle_aged"
+        return "senior"
+    
+    @computed_field
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+        
+# prediction endpoint
+@app.post('/predict')
 def predict_premium(data: UserInput):
-   input_df= pd.DataFrame([{
-      'bmi' : data.bmi,
-      'age_group' : data.age_group,
-      'lifestyle_risk': data.lifestyle_risk,
-      'city_tier': data.city_tier,
-      'income_lpa': data.income_lpa,
-      'occupation': data.occupation
 
-   }])
-   prediction = model.predict(input_df)[0]
-   return JSONResponse(status_code=200, content={'predicted_category': prediction})
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+    
+    prediction = model.predict(input_df)[0]
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
